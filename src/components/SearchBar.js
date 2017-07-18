49d7753df11905649b20724f7e42acae238c197f
Daniel Esquinazi <esquinazi.dani@gmail.com>
2017-07-18 19:30:35 -0300

    Fix identation

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,40 +1,48 @@
 import React from 'react';
-import { Header, Item, Input, Icon, Button, Text } from 'native-base';
+import {
+  Header,
+  Item,
+  Input,
+  Icon,
+  Button,
+  Text
+} from 'native-base';
 
-export const SearchBar = (props) => { 
-    const { onBackButtonClicked, handleOnTextChanged } = props;
-    const clearText = () => {
-     this.textInput.setNativeProps({ text: '' });
-    }
-    return (
-            <Header searchBar rounded>
-                <Item>
-                    <Icon 
-                      name="arrow-back" 
-                      onPress={() => {
-                        onBackButtonClicked();
-                        handleOnTextChanged('');
-                      }}
-                    />
-                    <Input
-                      ref={(component) => { this.textInput = component; }}
-                      placeholder="Search" 
-                      onChangeText={(text) => {
-                      handleOnTextChanged(text);
-                      }}
-                    />
-                    <Icon 
-                      name="close" 
-                      onPress={() => {
-                        clearText();
-                        handleOnTextChanged('');
-                      }}
-                    />
-                </Item>
-                
-                <Button transparent>
-                    <Text>Search</Text>
-                </Button>
-            </Header>
-    );
+export const SearchBar = (props) => {
+  const { onBackButtonClicked, handleOnTextChanged } = props;
+  const clearText = () => {
+    this.textInput.setNativeProps({ text: '' });
+  }
+  return (
+    <Header searchBar rounded>
+      <Item>
+        <Icon 
+          name="arrow-back" 
+          onPress={() => {
+            onBackButtonClicked();
+            handleOnTextChanged('');
+          }}
+        />
+        <Input 
+          ref={(component) => {
+            this.textInput = component;
+          }} 
+          placeholder="Search" 
+          onChangeText={(text) => {
+            handleOnTextChanged(text);
+          }}
+        />
+        <Icon 
+          name="close" 
+          onPress={() => {
+            clearText();
+            handleOnTextChanged('');
+          }}
+        />
+      </Item>
+      <Button transparent>
+        <Text>Search</Text>
+      </Button>
+    </Header>
+  );
 }
